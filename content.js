86c4bb1dc3afc4ca014aeb96cadcf02ca15d4a46
danthynguyen <danthynguyen@gmail.com>
2020-10-24 18:23:00 -0500

    counting covid

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-alert("Loading Covid Correcter")
+//alert("Loading Covid Correcter")
+chrome.runtime.onMessage.addListener(function (request ,sender, sendResponse) {
+    const findCovid = new RegExp('Covid', 'gi')
+    const matches = 
+    document.documentElement.innerHTML.match(findCovid)
+    sendResponse({count: matches.length})
+})
