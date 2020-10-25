94cd578792af50be2129efd211326fe2dabff084
William Eng <williame007@gmail.com>
2020-10-25 03:02:52 -0500

    Added article parsing functionality

--- popup.js
+++ popup.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     function onclick(){
         chrome.tabs.query({currentWindow: true, active: true},
             function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, {msg: 'runCovidCounter'}, setCount)
+                chrome.tabs.sendMessage(tabs[0].id, {msg: 'tryGetArticle'}, setCount)
             })
     }
     function setCount(res){
         const div = document.createElement('div')
-        div.textContent = `${res.count} Covid mentions found`
+        //div.textContent = `${res.count} Covid mentions found`
         document.body.appendChild(div)
     }
 }, false)
